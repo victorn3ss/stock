92088dbbc361837e8ac46a28c9c1601ff659d8c0
victorn3ss <100310151+victorn3ss@users.noreply.github.com>
2023-02-17 18:52:33 +0700

    first commit

---
+++ application/models/User_Model.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class User_Model extends CI_Model {
+
+
+    }
---
+++ application/views/1main.php
@@ -0,0 +1,102 @@
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css?family=Sarabun:300&amp;display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="<?=base_url('assets/fonts/icomoon/style.css')?>">
+        <link rel="stylesheet" href="<?=base_url('assets/css/owl.carousel.min.css')?>">
+        <link rel="stylesheet" href="<?=base_url('assets/css/bootstrap.min.css')?>">
+        <link rel="stylesheet" href="<?=base_url('assets/css/style.css')?>">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+        <title>Sidebar #8</title>
+    </head>
+    <body>
+        <aside class="sidebar">
+            <div class="toggle">
+                <a href="#" class="burger js-menu-toggle" data-toggle="collapse" data-target="#main-navbar">
+                    <span></span>
+                </a>
+            </div>
+            <div class="side-inner">
+                <div class="profile">
+                    <h3 class="name">Hi, Nuch</h3>
+                </div>
+                <div class="nav-menu">
+                    <ul>
+                        <li><a href="#"><i class="bi bi-house"></i> Home</a></li>
+                        <li><a href="#"><i class="bi bi-graph-down"></i> Summary</a></li>
+                        <li><a href="#"><i class="bi bi-box-seam"></i> Stock</a></li>
+                        <li><a href="#"><i class="bi bi-box-arrow-right"></i> Sign out</a></li>
+                    </ul>
+                </div>
+            </div>
+        </aside>
+        <main>
+            <div class="site-section">
+                <div class="container">
+                    <div class="row justify-content-center mt-0">
+                        <div class="mb-3">
+                            <h4>Product management</h4>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="mb-3">
+                            <input type="text" class="form-control" id="searchStock" placeholder="ค้นหาสิ่งที่ต้องการ">
+                        </div>
+                    </div>
+                    <div class="row justify-content-center mt-5">
+                        <div class="col-md-9">
+                            <div class="row">
+                                
+                                <div class="col-md-6">
+                                    <div class="d-flex post-entry">
+                                        <div class="custom-thumbnail">
+                                            <img src="https://www.hybrid-tobacco.com/wp-content/uploads/2020/10/S__8175650.jpg" alt="Image" class="img-fluid">
+                                        </div>
+                                        <div class="post-content">
+                                            <h3>Infinity Coke</h3>
+                                            <div class="post-meta"><span>remaining:</span> 40 item</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="d-flex post-entry">
+                                        <div class="custom-thumbnail">
+                                            <img src="https://www.hybrid-tobacco.com/wp-content/uploads/2020/12/3057675940_27161771456_SP0_SS_600x-300x300.jpg" alt="Image" class="img-fluid">
+                                        </div>
+                                        <div class="post-content">
+                                            <h3>Infinity Orange</h3>
+                                            <div class="post-meta"><span>remaining:</span> 40 item</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="d-flex post-entry">
+                                        <div class="custom-thumbnail">
+                                            <img src="https://www.hybrid-tobacco.com/wp-content/uploads/2020/10/S__8175654-300x300.jpg" alt="Image" class="img-fluid">
+                                        </div>
+                                        <div class="post-content">
+                                            <h3>Tiktok</h3>
+                                            <div class="post-meta"><span>remaining:</span> 40 item</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <script src="<?=base_url('assets/js/jquery-3.3.1.min.js')?>"></script>
+        <script src="<?=base_url('assets/js/popper.min.js')?>"></script>
+        <script src="<?=base_url('assets/js/bootstrap.min.js')?>"></script>
+        <script src="<?=base_url('assets/js/main.js')?>"></script>
+    </body>
+</html>
